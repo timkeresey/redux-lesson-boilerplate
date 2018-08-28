92d0511933b6cac0f12a45f947f0ad4873ed9b17
Christie Lynam <christielynam@gmail.com>
2018-08-27 18:55:45 -0600

    Update todosReducer

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -4,10 +4,9 @@ export const todosReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'ADD_TODO':
       return [...state, {text: action.text, id: action.id, completed: false}];
-      break;
     case 'TOGGLE_TODO':
       return state.map(todo => {
-        return (todo.id === action.id) ? {...todo, completed: !todo.completed} : todo
+        return todo.id === action.id ? {...todo, completed: !todo.completed} : todo
       })
     default:
       return state
